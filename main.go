0d727564858ccc0d7ff10e9d004ce27278511c67
sudityashrivastav <157526569+anonymouseshrivastav@users.noreply.github.com>
2024-01-27 18:13:11 +0530

    v2.1

--- main.go
+++ main.go
@@ -12,8 +12,11 @@ import (
 )
 
 var (
+	mutex               sync.Mutex
 	wg                  sync.WaitGroup
-	status_codes_string = "404"
+	status_codes_string     = "404"
+	errors              int = 0
+	checkedLines        int = 0
 )
 
 func main() {
@@ -47,12 +50,13 @@ func main() {
 	}
 	defer file.Close()
 
+	totalLines := checkTotalLines(fileLocation)
 	scanner := bufio.NewScanner(file)
-	fmt.Println("\033[36m", "Status Code\tURL", "\033[0m")
+	fmt.Println("\033[36m", "Code:Length\tProcess\tErrors\tURL", "\033[0m")
 	for scanner.Scan() {
 		word := scanner.Text()
 		wg.Add(1)
-		go start(URL, word, semaphore, status_codes)
+		go start(URL, word, semaphore, status_codes, totalLines)
 	}
 
 	wg.Wait()
@@ -60,11 +64,33 @@ func main() {
 	fmt.Println("Fuzzing Completed..")
 }
 
-func start(url string, word string, semaphore chan struct{}, status_codes []string) {
+func banner() {
+	fmt.Println("\033[31m", `
+███████╗██╗   ██╗███████╗███████╗███████╗██████╗
+██╔════╝██║   ██║╚══███╔╝╚══███╔╝██╔════╝██╔══██╗
+█████╗  ██║   ██║  ███╔╝   ███╔╝ █████╗  ██████╔╝
+██╔══╝  ██║   ██║ ███╔╝   ███╔╝  ██╔══╝  ██╔══██╗
+██║     ╚██████╔╝███████╗███████╗███████╗██║  ██║
+╚═╝      ╚═════╝ ╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝`)
+	fmt.Println("\tby Anon Shrivastav\n", "\033[0m")
+}
+
+func usage(msg string) {
+	fmt.Println("\nError:\033[31m ", msg, "\033[0m")
+	fmt.Println("\033[32mfuzz <url> <wordlist> <threads> <negative status codes>")
+	fmt.Println("fuzz https://google.com wordlist.txt 40 404,500,302\033[0m")
+	os.Exit(0)
+}
+
+func start(url string, word string, semaphore chan struct{}, status_codes []string, totalLines int) {
 	defer wg.Done()
 
 	semaphore <- struct{}{}
 	defer func() { <-semaphore }()
+
+	mutex.Lock()
+	checkedLines++
+	mutex.Unlock()
 
 	if !strings.HasSuffix(url, "/") {
 		url = url + "/"
@@ -78,7 +104,9 @@ func start(url string, word string, semaphore chan struct{}, status_codes []stri
 	req, err := http.NewRequest("GET", address, nil)
 
 	if err != nil {
-		//	fmt.Println("Error at making request: ", err.Error())
+		mutex.Lock()
+		errors++
+		mutex.Unlock()
 		return
 	}
 
@@ -88,7 +116,9 @@ func start(url string, word string, semaphore chan struct{}, status_codes []stri
 	res, err := client.Do(req)
 
 	if err != nil {
-		// fmt.Println("Error at making request: ", err.Error())
+		mutex.Lock()
+		errors++
+		mutex.Unlock()
 		return
 	}
 	defer res.Body.Close()
@@ -102,29 +132,27 @@ func start(url string, word string, semaphore chan struct{}, status_codes []stri
 		}
 	}
 
+	checkedPercentage := (checkedLines * 100) / totalLines
 	if result {
+		clr := "\033[33m"
 		if res.StatusCode == 200 {
-			fmt.Println("\033[32m", res.StatusCode, "\t\t", address, "\033[0m")
-		} else {
-			fmt.Println("\033[33m", res.StatusCode, "\t\t", address, "\033[0m")
+			clr = "\033[32m"
 		}
+
+		fmt.Printf("%s%d:%d\t%d/%d (%d%%)\t%d\t%s\n\033[0m", clr, res.StatusCode, res.ContentLength, checkedLines, totalLines, checkedPercentage, errors, address)
 	}
+
 }
 
-func usage(msg string) {
-	fmt.Println("\nError:\033[31m ", msg, "\033[0m")
-	fmt.Println("\033[32mfuzz <url> <wordlist> <threads> <negative status codes>")
-	fmt.Println("fuzz https://google.com wordlist.txt 40 404,500,302\033[0m")
-	os.Exit(0)
-}
+func checkTotalLines(fileName string) int {
+	file, _ := os.Open(fileName)
 
-func banner() {
-	fmt.Println("\033[31m", `
-███████╗██╗   ██╗███████╗███████╗███████╗██████╗
-██╔════╝██║   ██║╚══███╔╝╚══███╔╝██╔════╝██╔══██╗
-█████╗  ██║   ██║  ███╔╝   ███╔╝ █████╗  ██████╔╝
-██╔══╝  ██║   ██║ ███╔╝   ███╔╝  ██╔══╝  ██╔══██╗
-██║     ╚██████╔╝███████╗███████╗███████╗██║  ██║
-╚═╝      ╚═════╝ ╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝`)
-	fmt.Println("\tby Anon Shrivastav\n", "\033[0m")
+	scanner := bufio.NewScanner(file)
+
+	count := 0
+	for scanner.Scan() {
+		count++
+	}
+
+	return count
 }
